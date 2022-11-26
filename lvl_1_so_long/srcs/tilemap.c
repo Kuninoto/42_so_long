4144fa8028e415a0cdc05428b09fbdeff2f6d53a
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r10s2.42porto.com>
2022-11-26 23:43:09 +0000

    Updates

--- lvl_1_so_long/srcs/tilemap.c
+++ lvl_1_so_long/srcs/tilemap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tilemap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 21:20:29 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/11/26 01:55:58 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/11/26 22:11:34 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,20 @@ void	open_xpm(t_game *game)
 
 	img_size = 64;
 	game->tiles.wall = mlx_xpm_file_to_image(game->mlx_ptr, WALL_TILE, &img_size, &img_size);
+	printf("OPEN WALL = %p\n", game->tiles.wall);
 	if (!game->tiles.wall)
 		handle_error("Couldn't open wall image");
 	game->tiles.floor = mlx_xpm_file_to_image(game->mlx_ptr, FLOOR_TILE, &img_size, &img_size);
+	printf("OPEN FLOOR = %p\n", game->tiles.floor);
 	if (!game->tiles.floor)
 		handle_error("Couldn't open floor image");
 	game->tiles.player = mlx_xpm_file_to_image(game->mlx_ptr, PLAYER_TILE, &img_size, &img_size);
 	if (!game->tiles.player)
 		handle_error("Couldn't open player image");
-
-/* 	game->tiles.enemy = mlx_xpm_file_to_image(game->mlx_ptr, ENEMY_TILE, &img_size, &img_size);
+	game->tiles.enemy = mlx_xpm_file_to_image(game->mlx_ptr, ENEMY_TILE, &img_size, &img_size);
+	printf("OPEN ENEMY = %p\n", game->tiles.enemy);
 	if (!game->tiles.enemy)
-		handle_error("Couldn't open enemy image"); */
-
+		handle_error("Couldn't open enemy image");
 	game->tiles.coin = mlx_xpm_file_to_image(game->mlx_ptr, COLLECTIBLE_TILE, &img_size, &img_size);
 	if (!game->tiles.coin)
 		handle_error("Couldn't open enemy image");
@@ -58,28 +59,29 @@ void	get_tilemap(t_game *game)
 		j = -1;
 		while (++j < game->map->columns)
 		{
-			if (game->map->map[i][j] == WALL || game->map->map[i][j] == OPEN_SPACE)
+			if (game->map->map[i][j] == WALL)
+			{
+				printf("wall = %p\n", game->tiles.wall);
 				game->tile_map[i][j].img = game->tiles.wall;
+			}
 			else if (game->map->map[i][j] == COLLECTIBLE)
 				game->tile_map[i][j].img = game->tiles.coin;
 			else if (game->map->map[i][j] == EXIT)
 				game->tile_map[i][j].img = game->tiles.exit;
-			/* else
-				game->tile_map[i][j].img = game->tiles.floor; */
+	 	 	else
+			{
+				printf("floor = %p\n", game->tiles.floor);
+				game->tile_map[i][j].img = game->tiles.floor;
+			}
 		}
-		// o tile que fica no else nao funciona nao sei por alma de quem. on press()
-		// tambem nao esta a funcionar, seg fault. acredito que seja pela forma como estou a passar o game duma func para a outra.
 	}
 	game->tile_map[i] = NULL;
 }
 
-
-// SOMETHING'S OFF HERE
 void	render_tilemap(t_game *game)
 {
 	int	i;
 	int	j;
-//	int dummy;
 
 	i = -1;
 	printf("RENDER_TILEMAP\n");
@@ -89,10 +91,7 @@ void	render_tilemap(t_game *game)
 		while (++j < game->map->columns)
 		{
 			printf("[%d][%d]\n", i, j);
-			/* if (i != game->map->player_coords.x || j != game->map->player_coords.y)
-				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, 
-					mlx_xpm_file_to_image(game->mlx_ptr, WALL_TILE, &dummy, &dummy), TILE_SIZE * j, TILE_SIZE * i); */
-			if (i != game->map->player_coords.x || j != game->map->player_coords.y || game->map->map[i][j] == OPEN_SPACE)
+			if (i != game->map->player_coords.x || j != game->map->player_coords.y)
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->tile_map[i][j].img, TILE_SIZE * j, TILE_SIZE * i);
 		}
 	}
