6320c369634935a362b3b2476a7701dbce18c853
Kuninoto <nunorebelocarvalho@sapo.pt>
2022-11-27 03:16:40 +0000

    Massive updates...

--- lvl_1_so_long/srcs/get_tilemap.c
+++ lvl_1_so_long/srcs/get_tilemap.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tilemap.c                                          :+:      :+:    :+:   */
+/*   get_tilemap.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/25 21:20:29 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/11/26 22:11:34 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/11/27 03:11:55 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,39 +34,39 @@ void	open_xpm(t_game *game)
 		handle_error("Couldn't open enemy image");
 	game->tiles.coin = mlx_xpm_file_to_image(game->mlx_ptr, COLLECTIBLE_TILE, &img_size, &img_size);
 	if (!game->tiles.coin)
-		handle_error("Couldn't open enemy image");
+		handle_error("Couldn't open collectible image");
 	game->tiles.exit = mlx_xpm_file_to_image(game->mlx_ptr, EXIT_TILE, &img_size, &img_size);
 	if (!game->tiles.exit)
 		handle_error("Couldn't open exit image");
 	printf("OPEN_XPM\n");
 }
 
-void	get_tilemap(t_game *game)
+void	init_tilemap(t_game *game)
 {
 	int		i;
 	int		j;
 
 	printf("GET_TILEMAP\n");
-	game->tile_map = malloc((game->map->rows + 1) * sizeof(t_tilemap *));
+	game->tile_map = malloc((game->map.rows + 1) * sizeof(t_tilemap *));
 	if (!game->tile_map)
 		handle_error("Failed to allocate memory for tilemap");
 	i = -1;
-	while (++i < game->map->rows)
+	while (++i < game->map.rows)
 	{
-		game->tile_map[i] = malloc(((game->map->columns) + 1) * sizeof(t_tilemap));
+		game->tile_map[i] = malloc(((game->map.columns) + 1) * sizeof(t_tilemap));
 		if (!game->tile_map[i])
 			handle_error("Failed to allocate memory for tilemap");
 		j = -1;
-		while (++j < game->map->columns)
+		while (++j < game->map.columns)
 		{
-			if (game->map->map[i][j] == WALL)
+			if (game->map.map[i][j] == WALL)
 			{
 				printf("wall = %p\n", game->tiles.wall);
 				game->tile_map[i][j].img = game->tiles.wall;
 			}
-			else if (game->map->map[i][j] == COLLECTIBLE)
+			else if (game->map.map[i][j] == COLLECTIBLE)
 				game->tile_map[i][j].img = game->tiles.coin;
-			else if (game->map->map[i][j] == EXIT)
+			else if (game->map.map[i][j] == EXIT)
 				game->tile_map[i][j].img = game->tiles.exit;
 	 	 	else
 			{
@@ -85,26 +85,26 @@ void	render_tilemap(t_game *game)
 
 	i = -1;
 	printf("RENDER_TILEMAP\n");
-	while (++i < game->map->rows)
+	while (++i < game->map.rows)
 	{
 		j = -1;
-		while (++j < game->map->columns)
+		while (++j < game->map.columns)
 		{
 			printf("[%d][%d]\n", i, j);
-			if (i != game->map->player_coords.x || j != game->map->player_coords.y)
+			if (i != game->map.player_x || j != game->map.player_y)
 				mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->tile_map[i][j].img, TILE_SIZE * j, TILE_SIZE * i);
 		}
 	}
-/* 	mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, 
-		game->tiles.player, TILE_SIZE * game->map->player_coords.y, 
-		TILE_SIZE * game->map->player_coords.x); */
+	mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, 
+		game->tiles.player, TILE_SIZE * game->map.player_y, 
+		TILE_SIZE * game->map.player_x);
 	printf("FINISH RENDER_TILEMAP\n");
 }
 
-void	init_tilemap(t_game *game)
+void	get_tilemap(t_game *game)
 {
 	printf("INIT_TILEMAP\n");
 	open_xpm(game);
-	get_tilemap(game);
+	init_tilemap(game);
 	render_tilemap(game);
 }
--- lvl_1_so_long/srcs/map/path_check.c
+++ lvl_1_so_long/srcs/map/path_check.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   path_check.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnuno-ca <nnuno-ca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nnuno-ca <nnuno-ca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 19:03:23 by nnuno-ca          #+#    #+#             */
-/*   Updated: 2022/11/23 17:55:48 by nnuno-ca         ###   ########.fr       */
+/*   Updated: 2022/11/27 03:12:05 by nnuno-ca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/so_long.h"
 
-int	**blank_grid(t_map *map)
+int	**blank_grid(t_game *game)
 {
 	int		**grid;
 	int		i;
 	int		j;
 
-	grid = malloc((map->rows + 1) * sizeof(int *));
+	grid = malloc((game->map.rows + 1) * sizeof(int *));
 	i = -1;
 	j = -1;
-	while (++i < map->rows)
+	while (++i < game->map.rows)
 	{
-		grid[i] = malloc((map->columns + 1) * sizeof(int));
+		grid[i] = malloc((game->map.columns + 1) * sizeof(int));
 		j = -1;
-		while (++j < map->columns)
+		while (++j < game->map.columns)
 			grid[i][j] = 0;
 		grid[i][j] = '\0';
 	}
@@ -33,41 +33,41 @@ int	**blank_grid(t_map *map)
 	return (grid);
 }
 
-bool	is_valid(t_map *map, int i, int j)
+bool	is_valid(t_game *game, int i, int j)
 {
-	if (i >= 0 && i < map->rows && j >= 0 && j < map->columns
-		&& map->map[i][j] != '1')
+	if (i >= 0 && i < game->map.rows && j >= 0 && j < game->map.columns
+		&& game->map.map[i][j] != '1')
 		return (true);
 	return (false);
 }
 
-int	find_path(t_map *map, int i, int j, int **sol_grid)
+int	find_path(t_game *game, int i, int j, int **sol_grid)
 {
-	if (map->map[i][j] == 'E')
-		map->exit_ok = true;
-	if (map->map[i][j] == 'C' && sol_grid[i][j] != 1)
-		map->c_oks++;
+	if (game->map.map[i][j] == 'E')
+		game->map.exit_ok = true;
+	if (game->map.map[i][j] == 'C' && sol_grid[i][j] != 1)
+		game->map.c_oks++;
 	if (sol_grid[i][j] == 1)
-		return (map->c_oks);
-	if (is_valid(map, i, j) == false)
-		return (map->c_oks);
+		return (game->map.c_oks);
+	if (is_valid(game, i, j) == false)
+		return (game->map.c_oks);
 	sol_grid[i][j] = 1;
-	map->c_oks = find_path(map, i + 1, j, sol_grid);
-	map->c_oks = find_path(map, i, j + 1, sol_grid);
-	map->c_oks = find_path(map, i - 1, j, sol_grid);
-	map->c_oks = find_path(map, i, j - 1, sol_grid);
-	return (map->c_oks);
+	game->map.c_oks = find_path(game, i + 1, j, sol_grid);
+	game->map.c_oks = find_path(game, i, j + 1, sol_grid);
+	game->map.c_oks = find_path(game, i - 1, j, sol_grid);
+	game->map.c_oks = find_path(game, i, j - 1, sol_grid);
+	return (game->map.c_oks);
 }
 
-void	path_check(t_map *map)
+void	path_check(t_game *game)
 {
 	int		**sol_grid;
 
-	sol_grid = blank_grid(map);
-	if (find_path(map, map->player_coords.x, map->player_coords.y, sol_grid)
-		!= map->collectibles)
+	sol_grid = blank_grid(game);
+	if (find_path(game, game->map.player_x, game->map.player_y, sol_grid)
+		!= game->map.collectibles)
 		handle_error("Unachievable Collectibles (C) in the map");
 	free(sol_grid);
-	if (map->exit_ok == false)
+	if (game->map.exit_ok == false)
 		handle_error("Map's Exit (E) is unachievable");
 }
